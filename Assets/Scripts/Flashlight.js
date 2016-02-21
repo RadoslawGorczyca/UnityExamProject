1f757ce50c7eb805c14e6035dbe76b6ed52f11a3
d33zer <d33zer@interia.pl>
2016-02-21 10:42:10 +0100

    Latarka

---
+++ Assets/Scripts/Flashlight.js
@@ -0,0 +1,8 @@
+﻿function Update() {
+    if (Input.GetKeyDown("f")) {
+        if (GetComponent.<Light>().enabled == true)
+            GetComponent.<Light>().enabled = false;
+        else
+            GetComponent.<Light>().enabled = true;
+    }
+}
